9b861e3c487b8ee4c6514596d1b450510325799b
Kirill <kllraz@yandex.ru>
2025-07-04 17:41:38 +0300

    fix code by linter

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, FastAPI
 
 router = APIRouter()
 
+
 @router.get("/")
-async def hello():
+async def hello() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 
 
-def test_hello(client: TestClient):
+def test_hello(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
